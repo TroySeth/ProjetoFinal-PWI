8e4e9bcf68c6d22922d0224c547bd9323091d415
leomendes18 <leomendesmonteiro1@gmail.com>
2023-02-01 10:00:37 -0300

    criando a função de update e delete dos subject e as rotas

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -17,9 +17,31 @@ async function create (req, res){
     }
 };
 
+// Função de pegar todos os assuntos do fórum no banco
 async function findAll (req, res){
     const subjectAll = await subjectModel.find()
     res.send(subjectAll)
 };
 
-module.exports = {create, findAll}
+async function editSubject (req, res){
+    try{
+        await subjectModel.updateOne({_id: req.body.id},{title: req.body.title, content: req.body.content});
+        setTimeout(function() {
+            res.status(201).json('subject do ID: ' + req.body.id + ' foi modificado')
+            }, 1000);
+    } catch(error){
+        console.log('Falha ao editar assunto: ' + error)
+    }
+};
+
+async function destroySubject (req, res){
+    await subjectModel.deleteOne({_id: req.body.id}).then(() => {
+        setTimeout(function() {
+            res.status(200).json('subject do ID: ' + req.body.id + ' foi deletado')
+        }, 500);
+    }).catch((error) => {
+        console.log('Erro ao deletar assunto.' + error);
+    })
+};
+
+module.exports = {create, findAll, editSubject, destroySubject}
--- routes/routes.js
+++ routes/routes.js
@@ -5,5 +5,7 @@ const subjectController = require('../controllers/subjectController')
 // Routes
 router.route('/').get((req, res) => subjectController.findAll(req, res));
 router.route('/').post((req, res) => subjectController.create(req, res));
+router.route('/update').put((req, res) => subjectController.editSubject(req , res));
+route.route('/delete').delete((req, res) => subjectController.destroySubject(req, res));
 
 module.exports = router;
