7a3e6f116ae85ad4c41368d3b4da4c6c26a23d37
leomendes18 <leomendesmonteiro1@gmail.com>
2023-02-02 09:38:05 -0300

    testando

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 
 const subjectController = require('../controllers/subjectController')
+const commentController = require('../controllers/commentController')
+const topicController = require('../controllers/topicController')
 
 // Routes
 router.route('/buscar').get((req, res) => subjectController.findAll(req, res));
 router.route('/').post((req, res) => subjectController.create(req, res));
 router.route('/update').put((req, res) => subjectController.editSubject(req , res));
 router.route('/delete').delete((req, res) => subjectController.destroySubject(req, res));
+router.route('')
 
 module.exports = router;
