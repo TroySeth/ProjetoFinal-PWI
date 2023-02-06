26eae7621fc56a621396bb35b774cfdec0c370f5
Guilherme Costa <gui-costaquino@hotmail.com>
2023-02-05 23:35:06 -0300

    Modificando view de fazer post e sua rota

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,7 @@ const userController = require('../controllers/userController');
 
 // Routes do post
 router.get('/', postController.findAll);
+router.get('/post', userController.isAuthenticated, (req, res) => res.render('partials/post'));
 router.post('/post', userController.isAuthenticated, (req, res) => postController.create);
 router.put('/update', postController.editPost);
 router.delete('/delete', postController.destroyPost);
