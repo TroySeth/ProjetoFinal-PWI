19559b382faa120a4e9020c1f65f224bf2360f73
leomendes18 <leomendesmonteiro1@gmail.com>
2023-02-08 11:29:34 -0300

    removendo authenticated da rota posts

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,7 @@ const userController = require('../controllers/userController');
 // Routes do post
 router.get('/', (req, res) => res.render('partials/main/initial',{layout:'main'}));
 router.get('/post', userController.isAuthenticated, (req, res) => res.render('partials/post/initialPost',{layout:'post'}));
-router.get('/posts', userController.isAuthenticated, (req, res) => postController.findAll(req, res));
+router.get('/posts', (req, res) => postController.findAll(req, res));
 router.post('/post', userController.isAuthenticated, postController.create);
 router.put('/update', postController.editPost);
 router.delete('/delete', postController.destroyPost);
